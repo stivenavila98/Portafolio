6e729dcff39f22c7d352f3b0e9a900c3d11ceaee
stiven <stivenavila020498@gmail.com>
2022-10-25 20:48:32 -0400

    Minimal fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,6 @@ import {TranslateService} from '@ngx-translate/core';
 	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	title = 'AppLimpia';
-
 	constructor(public translate: TranslateService) {
 		this.setAppLanguage();
 	}
